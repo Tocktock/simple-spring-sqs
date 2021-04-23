38b83e00c5d90595627ad8a74699a21f5a91d417
Tocktock <qnfmtm666@naver.com>
2021-04-23 16:24:05 +0900

    delete test & add task-definition.json

--- src/main/kotlin/com/example/aws/HelloController.kt
+++ src/main/kotlin/com/example/aws/HelloController.kt
@@ -1,4 +1,5 @@
 package com.example.aws
+
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -8,38 +9,8 @@ import org.springframework.web.bind.annotation.RestController
 class HelloController {
 
     @GetMapping("/")
-    fun home() : String {
-        return "this is home"
+    fun home(): String {
+        return "s3-sns-sqs-s3 testing server"
     }
 
-    @GetMapping("/itsmylife")
-    fun itsnowornever() : String {
-        return "i'm gonna live for ever"
-    }
-
-    @GetMapping("/hello-world")
-    fun helloWorldMapping() : String {
-        return "Hello World!!"
-    }
-
-    @GetMapping("/lat-lon")
-    fun getLonLat() : String {
-        return "111.00 ,111.00"
-    }
-    //test sadfasdf
-
-    @GetMapping("/sendy")
-    fun getsendy() : String {
-        return "sendy"
-    }
-
-    @GetMapping("/itshard")
-    fun dohard() : String {
-        return "수고했어"
-    }
-
-    @GetMapping("/myload")
-    fun loadBalanced() : String {
-        return "load balanced page"
-    }
 }
